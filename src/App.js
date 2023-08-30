1cffd58b1bc6a3e43a46d5b04d3d17ae92958e1f
vrushabhjv <vrushabhjv2@gmail.com>
2023-08-30 20:52:49 +0530

    Title changed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">WELCOME TO OUR PRODUCT</h1>
+          <h1 className="App-title">WELCOME TO OUR PRODUCT EAGLEI</h1>
         </header>
         <p className="App-intro">
           Hello to our demo
