0890d8a1fff6ea04a5ee361a88a3b3073ba97f30
vrushabhjv <vrushabhjv2@gmail.com>
2023-08-30 20:40:39 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">WELCOME TO EAGLEIY</h1>
+          <h1 className="App-title">WELCOME TO OUR PRODUCT</h1>
         </header>
         <p className="App-intro">
           Hello to our demo
