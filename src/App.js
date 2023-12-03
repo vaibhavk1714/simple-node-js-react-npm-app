76f1b28a956e8979de3929002a5e6596e02636be
vaibhavk1714 <94043467+vaibhavk1714@users.noreply.github.com>
2023-12-03 19:25:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">WELCOME TO OUR PRODUCT EAGLEI</h1>
         </header>
         <p className="App-intro">
-           welcome to our demo
+           Welcome to our demo
         </p>
       </div>
     );
